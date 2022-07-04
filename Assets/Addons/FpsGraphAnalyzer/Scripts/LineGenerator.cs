070774954ed1c0f2d0f05a80d50e2ab74a4d7f9d
noradninja <josh@whitespacecg.com>
2022-07-04 15:53:58 -0400

    Push Up

---
+++ Assets/Addons/FpsGraphAnalyzer/Scripts/LineGenerator.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * Draws a graph of lines by means of OnGUI 
+ */ 
+public class LineGenerator : MonoBehaviour
+{
+    public Text fpsText;
+    [SerializeField]
+    Texture2D _texture;
+    public Color fpsGraphColor = Color.green;
+    [Tooltip("High value is considered to be any value grater 60fps")]
+    public Color fpsHighLineIndicatorColor = Color.yellow;
+    [Tooltip("Normal value is considered to be 60fps")]
+    public Color fpsNormalLineIndicatorColor = Color.red;
+
+    private const float TOP_NORMAL_MARGIN_SCALE = 0.54f;
+    private const float TOP_MAX_MARGIN_SCALE = 0.0f;
+    private const float FPS_NORMAL = 30f;
+    private const int MIN_SIZE = 16;
+
+    private int _amount = 100;
+    private float _deltaTime = 0.0f;
+    private string _fpsString = "";
+   public float _fullHeight = 0f;
+    private float _maxHeight = 0f;
+    private bool _canStart = false;
+    public Queue<float> _heightsQueue;
+    private Vector2 _bottomLeft = new Vector2(0f, 0f);
+    
+
+    public void Start()
+    {
+        StartCoroutine("InitializationRoutine");
+    }
+    
+    public IEnumerator InitializationRoutine()
+    {
+        // Debug.Log("Routine started, we are going to skeep a frame and then come back");
+        yield return null;
+
+        // Debug.Log("One frame is skipped.");
+
+        RectTransform rect = GetComponent<RectTransform>();
+
+        /*
+        Debug.Log("Rectangle width= " + rect.rect.width);
+        Debug.Log("Rectangle height= " + rect.rect.height);
+        Debug.Log("Offset of the Lower left corner of the rectangle relative to the lower left anchor= " + rect.offsetMin);
+        Debug.Log("Offset of the Upper right corner of the rectangle relative to the upper right anchor= " + rect.offsetMax);
+        */
+
+        _bottomLeft = rect.offsetMin;
+           
+        if (rect.rect.width < MIN_SIZE || rect.rect.width < MIN_SIZE)
+        {
+            throw new System.Exception("You need to specify Width and Height in Editor for the LineRenderer");
+        }
+
+        _amount = (int)rect.rect.width;
+        _fullHeight = rect.rect.height - (rect.rect.height * TOP_NORMAL_MARGIN_SCALE);
+        _maxHeight = rect.rect.height - (rect.rect.height * TOP_MAX_MARGIN_SCALE);
+        InitHeightsQueue();
+                                  
+        //Sets the position of Fps text field
+        fpsText.GetComponent<RectTransform>().offsetMin = rect.offsetMin;
+        fpsText.GetComponent<RectTransform>().offsetMax = rect.offsetMax;
+
+        //Debug.Log("Reached end of routine");
+        _canStart = true;
+    }
+
+    /**
+     * Creates a queue of zeroes with a size of LineRenderer image width
+     */ 
+    private void InitHeightsQueue()
+    {
+        _heightsQueue = new Queue<float>();
+
+        for (int i = 0; i < _amount; i++)
+        {
+            _heightsQueue.Enqueue(0f);
+        }
+    }
+
+    public void Update()
+    {
+        if (_canStart)
+        {
+            if (
+                    (Shader_LOD_Enumerator.tick == 4) || 
+                    (Shader_LOD_Enumerator.tick == 8) ||
+                    (Shader_LOD_Enumerator.tick == 12)
+                )
+            {
+                //Calculate FPS
+
+                float fps = FPS_Counter.averageFPS;
+                // _fpsString = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+                //
+                // //Display in Text field in Canvas
+                // if (fpsText != null)
+                // {
+                //     fpsText.text = _fpsString;
+                // }
+
+                //Remove first line in order and add a new line to the end
+                _heightsQueue.Dequeue();
+                float
+                    lineHeight =
+                        _fullHeight * fps /
+                        FPS_NORMAL; // where fps / FPS_NORMAL is a coefficient of current fps value to normal 30 fps
+
+                if (lineHeight > _maxHeight)
+                {
+                    lineHeight = _maxHeight;
+                }
+
+                _heightsQueue.Enqueue(lineHeight);
+                // _heightsQueue.Enqueue(0);
+            }
+            else
+            {
+                _heightsQueue.Dequeue();
+                float
+                    lineHeight = 0;
+                    _heightsQueue.Enqueue(lineHeight);
+            }
+        }
+    }
+
+    /**
+     * Draws texture with a size 2x2 px in a rectangle with a size 1xlineHeight px 
+     */
+    public void OnGUI()
+    {
+        if (_canStart)
+        {
+            
+            float counter = 0;
+            foreach (float lineHeight in _heightsQueue)
+            {
+                // Position of GUI layer is top left corner of the screen
+                float posX = counter + _bottomLeft.x;
+                float posY = Screen.height - _bottomLeft.y - lineHeight;
+                if (lineHeight <= 0) GUI.color = Color.black;
+                else if (lineHeight < _fullHeight-2) GUI.color = Color.red;
+                else  GUI.color = fpsGraphColor;
+                // if (Shader_LOD_Enumerator.tick == 15)
+                // {
+                    GUI.DrawTexture(new Rect(new Vector2(posX, posY), new Vector2(2, lineHeight)), _texture);
+                // }
+                counter++;
+            }
+            DrawNormalFpsLineIndicator();
+        }
+    }
+
+    /**
+     * Draws a normal fps line indicator with width = rectangle width, height=1px
+     * and a high fps line indicator above it.
+     * Both lines must be drawn above the fps graph rectangular area.
+    */
+    private void DrawNormalFpsLineIndicator()
+    {
+        GUI.color = fpsNormalLineIndicatorColor;
+        GUI.DrawTexture(new Rect(new Vector2(_bottomLeft.x, Screen.height - _bottomLeft.y - _fullHeight), new Vector2((float)_amount+2, 1)), _texture);
+
+        GUI.color = fpsHighLineIndicatorColor;
+        GUI.DrawTexture(new Rect(new Vector2(_bottomLeft.x, Screen.height - _bottomLeft.y - _maxHeight), new Vector2((float)_amount+2, 1)), _texture);
+
+        GUI.color = fpsGraphColor;
+        GUI.DrawTexture(new Rect(new Vector2(_bottomLeft.x,  58), new Vector2((float)_amount+2, 1)), _texture);
+    }
+}
---
+++ Assets/Script/PostProcess/Gamma.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+[ExecuteInEditMode]
+[AddComponentMenu("Image Effects/Color Adjustments/Gamma")]
+public class Gamma : PostEffectsBase {
+	public enum ColorCorrectionMode {
+		Simple = 0,
+		Advanced = 1
+	}
+
+	[HideInInspector]
+	public AnimationCurve channels = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
+	public bool useDepthCorrection = true;
+
+	private Material ccMaterial;
+	private Material ccDepthMaterial;
+	private Material selectiveCcMaterial;
+
+	private Texture2D rgbChannelTex;
+	private Texture2D rgbDepthChannelTex;
+	private Texture2D zCurveTex;
+
+	public float gamma = 1.0f;
+
+	[HideInInspector]
+	public ColorCorrectionMode mode;
+	[HideInInspector]
+	public bool updateTextures = true;
+
+	public Shader colorCorrectionCurvesShader = null;
+	public Shader simpleColorCorrectionCurvesShader = null;
+
+	private bool updateTexturesOnStartup = true;
+	
+	float oldGamma;
+
+
+	new void Start() {
+		base.Start();
+		updateTexturesOnStartup = true;
+		oldGamma = -1f;
+	}
+
+	void Awake() { }
+
+
+	public override bool CheckResources() {
+		CheckSupport(mode == ColorCorrectionMode.Advanced);
+
+		ccMaterial = CheckShaderAndCreateMaterial(simpleColorCorrectionCurvesShader, ccMaterial);
+		ccDepthMaterial = CheckShaderAndCreateMaterial(colorCorrectionCurvesShader, ccDepthMaterial);
+
+		if(!rgbChannelTex)
+			rgbChannelTex = new Texture2D(256, 4, TextureFormat.ARGB32, false, true);
+		if(!rgbDepthChannelTex)
+			rgbDepthChannelTex = new Texture2D(256, 4, TextureFormat.ARGB32, false, true);
+		if(!zCurveTex)
+			zCurveTex = new Texture2D(256, 1, TextureFormat.ARGB32, false, true);
+
+		rgbChannelTex.hideFlags = HideFlags.DontSave;
+		rgbDepthChannelTex.hideFlags = HideFlags.DontSave;
+		zCurveTex.hideFlags = HideFlags.DontSave;
+
+		rgbChannelTex.wrapMode = TextureWrapMode.Clamp;
+		rgbDepthChannelTex.wrapMode = TextureWrapMode.Clamp;
+		zCurveTex.wrapMode = TextureWrapMode.Clamp;
+
+		if(!isSupported)
+			ReportAutoDisable();
+		return isSupported;
+	}
+
+	public void UpdateParameters() {
+		CheckResources(); // textures might not be created if we're tweaking UI while disabled
+
+		if(channels != null) {
+			for(float i = 0.0f;i <= 1.0f;i += 1.0f / 255.0f) {
+				float ch = Mathf.Clamp(channels.Evaluate(i), 0.0f, 1.0f);
+
+				rgbChannelTex.SetPixel((int)Mathf.Floor(i * 255.0f), 0, new Color(ch, ch, ch) * gamma);
+				rgbChannelTex.SetPixel((int)Mathf.Floor(i * 255.0f), 1, new Color(ch, ch, ch) * gamma);
+				rgbChannelTex.SetPixel((int)Mathf.Floor(i * 255.0f), 2, new Color(ch, ch, ch) * gamma);
+
+				float zC = Mathf.Clamp(channels.Evaluate(i), 0.0f, 1.0f);
+
+				zCurveTex.SetPixel((int)Mathf.Floor(i * 255.0f), 0, new Color(zC, zC, zC) * gamma);
+
+				ch = Mathf.Clamp(channels.Evaluate(i), 0.0f, 1.0f);
+
+				rgbDepthChannelTex.SetPixel((int)Mathf.Floor(i * 255.0f), 0, new Color(ch, ch, ch) * gamma);
+				rgbDepthChannelTex.SetPixel((int)Mathf.Floor(i * 255.0f), 1, new Color(ch, ch, ch) * gamma);
+				rgbDepthChannelTex.SetPixel((int)Mathf.Floor(i * 255.0f), 2, new Color(ch, ch, ch) * gamma);
+			}
+
+			rgbChannelTex.Apply();
+			rgbDepthChannelTex.Apply();
+			zCurveTex.Apply();
+		}
+	}
+	
+	void Update(){
+		if(oldGamma != gamma){
+			oldGamma = gamma;
+			UpdateParameters();
+		}
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		if(CheckResources() == false) {
+			Graphics.Blit(source, destination);
+			return;
+		}
+
+		if(updateTexturesOnStartup) {
+			UpdateParameters();
+			updateTexturesOnStartup = false;
+		}
+
+		if(useDepthCorrection)
+			GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+
+		RenderTexture renderTarget2Use = destination;
+
+		if(useDepthCorrection) {
+			ccDepthMaterial.SetTexture("_RgbTex", rgbChannelTex);
+			ccDepthMaterial.SetTexture("_ZCurve", zCurveTex);
+			ccDepthMaterial.SetTexture("_RgbDepthTex", rgbDepthChannelTex);
+			ccDepthMaterial.SetFloat("_Saturation", 1f);
+
+			Graphics.Blit(source, renderTarget2Use, ccDepthMaterial);
+		}
+		else {
+			ccMaterial.SetTexture("_RgbTex", rgbChannelTex);
+			ccMaterial.SetFloat("_Saturation", 1f);
+
+			Graphics.Blit(source, renderTarget2Use, ccMaterial);
+		}
+	}
+}
