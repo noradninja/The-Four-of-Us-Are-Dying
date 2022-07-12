e92356eb4a02eb4e0e2c28a8bdc32e16bffa46b6
Noradninja <josh@whitespacecg.com>
2022-07-11 20:01:02 -0400

    Add singleton Input Manager

---
+++ Assets/Script/Tools/VitaDebug.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class VitaDebug  
+{
+#if UNITY_PSP2
+    [DllImport("PrintfExport")]
+    private static extern void print(string text);
+#endif
+
+    public static void Log(string log)
+    {
+        if(Application.platform.Equals(RuntimePlatform.PSP2)) print("[Unity PSP2]  " + log  + "\n");
+        else UnityEngine.Debug.Log(log);
+    }
+}
