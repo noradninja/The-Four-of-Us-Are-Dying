8a424f040661a3767ea869fd31f148838f254ac5
Josh Allen <josh@whitespacecg.com>
2023-06-22 15:00:26 -0400

    Remote Sync

---
+++ Assets/Script/Enumerators/Render_Pipeline_Overlay.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Render_Pipeline_Overlay : MonoBehaviour
+{
+	public Component [] components;
+	public Component currentComponent;
+	public Component previousComponent;
+	public RenderTexture[] renderTextures;
+	public RenderTexture currentRenderTexture;
+	public RenderTexture previousRenderTexture;
+	public RawImage overlayImage;
+	public RawImage overlayBG;
+	public int i = 0;
+	private int j = 0;
+	private int componentLength;
+	private int renderTextureLength;
+	
+	private bool firstPress = false;
+	
+	void Start ()
+	{
+		VitaInputManager.Instance.OnDpadRight += OnDpadRightEvent;
+		if (currentComponent == null) currentComponent = components[0] ;
+		if (currentRenderTexture == null) currentRenderTexture = renderTextures[0];
+		EnableAllComponents();		
+		componentLength = components.Length;
+		renderTextureLength = renderTextures.Length;
+		// overlayBG.color = Color.clear;
+		// overlayImage.color = Color.clear;
+	}
+
+	private void Update()
+	{
+		// if (i == 0)
+		// {
+		// 	overlayBG.color = Color.clear;
+		// 	overlayImage.color = Color.clear;
+		// }
+	}
+
+	private void OnDpadRightEvent()
+	{
+		if (!firstPress)
+		{
+			DisableAllComponents();
+		}
+		else if (firstPress)
+		{
+			if (i > componentLength-1)
+			{
+				DisableAllComponents();
+			}
+			else if (i < componentLength)
+			{
+				previousComponent = currentComponent;
+				currentComponent = components[i];
+				var currentComp = currentComponent.GetType();
+				((MonoBehaviour)GetComponent(currentComp)).enabled = true;
+				i++;
+			}
+
+			// if (j > renderTextureLength - 1)
+			// {
+			// 	j = 0;
+			// 	overlayImage.texture = null;
+			// 	previousRenderTexture = null;
+			// 	overlayBG.color = Color.clear;
+			// 	overlayImage.color = Color.clear;
+			// }
+			// else if (j < renderTextureLength)
+			// {
+			// 	overlayImage.color = Color.white;
+			// 	overlayBG.color = new Color(0, 0, 0, 0.5f);
+			// 	previousRenderTexture = currentRenderTexture;
+			// 	currentRenderTexture = renderTextures[j];
+			// 	overlayImage.texture = currentRenderTexture;
+			// }
+		}
+		
+	}
+	private void DisableAllComponents()
+	{
+		i = 0;
+		currentComponent = components[i];
+		previousComponent = null;
+		foreach (Component component in components)
+		{
+			var type = component.GetType();
+			((MonoBehaviour)GetComponent(type)).enabled = false;
+		}
+		if (!firstPress)
+		{
+			firstPress = true;
+		}
+	}
+	private void EnableAllComponents()
+	{
+		i = 0;
+		currentComponent = components[i];
+		previousComponent = null;
+		
+		foreach (Component component in components)
+		{
+			var type = component.GetType();
+			((MonoBehaviour)GetComponent(type)).enabled = true;
+		}
+	}
+}
---
+++ Assets/Shader/PostProcess/EAAA.shader
@@ -0,0 +1,84 @@
+﻿Shader "Vita/Edge-Aware Adaptive Anti-Aliasing"
+{
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+		Pass
+		{
+			Name "Edge-Aware Adaptive Anti-Aliasing"
+			LOD 100
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#define MIPMAP_LEVEL 2
+			#define EDGE_THRESHOLD 0.1
+
+			float EdgeDetection(sampler2D mainTex, float2 uv, float2 texelSize)
+			{
+				float4 centerColor = tex2D(mainTex, uv);
+				float4 dx = ddx(centerColor);
+				float4 dy = ddy(centerColor);
+				float edgeFactor = max(dot(dx, dx), dot(dy, dy));
+				edgeFactor = saturate((edgeFactor - EDGE_THRESHOLD) / (1.0 - EDGE_THRESHOLD));
+				return edgeFactor;
+			}
+
+			float4 EdgeSmoothing(sampler2D mainTex, float2 uv, float2 texelSize, float edgeFactor)
+			{
+				float4 centerColor = tex2D(mainTex, uv);
+				float2 offsets[4] = 
+				{
+					float2(-texelSize.x, -texelSize.y),
+					float2(texelSize.x, -texelSize.y),
+					float2(-texelSize.x,  texelSize.y),
+					float2(texelSize.x,  texelSize.y)
+				};
+
+				float4 blendedColor = centerColor;
+				for (int i = 0; i < 4; ++i)
+				{
+					float4 neighborColor = tex2D(mainTex, uv + offsets[i]);
+					blendedColor += neighborColor * edgeFactor;
+				}
+
+				blendedColor /= (4.0 * edgeFactor + 1.0);
+				return blendedColor;
+			}
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float2 texelSize : TEXCOORD1;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _AntiAliasingTarget;
+			float4 _AntiAliasingTarget_TexelSize;
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = float4(v.vertex.xy, 0.0, 1.0);
+				o.uv = v.vertex.xy * 0.5 + 0.5;
+				o.texelSize = _AntiAliasingTarget_TexelSize.xy;
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed edgeFactor = EdgeDetection(_AntiAliasingTarget, i.uv, i.texelSize);
+				fixed4 color = EdgeSmoothing(_AntiAliasingTarget, i.uv, i.texelSize, edgeFactor);
+				return color;
+			}
+			ENDCG
+		}
+
+	}
+}
