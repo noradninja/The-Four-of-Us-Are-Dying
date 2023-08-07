4ec63dea96d03f351277c77ac5fe5f880a3e3f16
Josh Allen <josh@whitespacecg.com>
2023-08-07 13:53:49 -0400

    Add inbuilt OC, test native PSP res perf for 0.2a

---
+++ Assets/Script/Tools/UnityOC.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+class UnityOC 
+{
+    private static readonly int DefaultArm = 333;
+    private static readonly int DefaultBus = 111;
+    private static readonly int DefaultGPU = 111;
+    private static readonly int DefaultXbar = 111;
+
+    [DllImport("libUnityOC")]
+    public static extern int SetFrequencies(int cpu, int bus, int gpu, int xbar);
+
+    public static void ResetFrequencies()
+    {
+        SetFrequencies(DefaultArm, DefaultBus, DefaultGPU, DefaultXbar); // Just to set them back
+        SetFrequencies(-1, -1, -1, -1); // Now this will release our hooks, giving back control to Unity (or whatever controls clocks in this context)
+    }
+}
